76da55e33876e9ac5e00083ea404bff71ab76fcc
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-11-28 16:30:36 +0200

    Downgrade to ChakraUI v2

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "@/components/ui/provider";
+// import { Provider } from "@/components/ui/provider";
 import "./index.css";
 import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </StrictMode>
 );
