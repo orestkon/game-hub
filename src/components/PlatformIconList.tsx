a744c16a427ad7f3c1f4381832f462e4a6491656
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-12-03 18:59:46 +0200

    Building platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import {
+  FaWindows,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+  FaPlaystation,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { type Platform } from "@/hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+import type { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    mac: FaApple,
+    linux: FaLinux,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug]}
+          color={"gray.500"}
+        />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
