aaf6f4f70158b9bda5392474d0e2c559a55e339f
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-12-03 21:15:51 +0200

    Fetch genre list in the menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
       </GridItem>
 
       <GridItem area={"aside"} hideBelow={"lg"}>
-        Aside
+        <GenreList />
       </GridItem>
       <GridItem area={"main"}>
         <GameGrid />
