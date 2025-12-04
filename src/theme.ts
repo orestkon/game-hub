802f2b583759408924021d59ca0f3dd791a1ef61
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2025-12-04 23:16:05 +0200

    Theme configurations

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,11 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
 
 const theme = extendTheme({
+  config,
   colors: {
     gray: {
       50: "#f9f9f9",
